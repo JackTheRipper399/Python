1efd1b64ce199392b409893c70073d3a2ed14967
JackTheRipper399 <mike.morin.alsbury@gmail.com>
2025-07-09 11:27:25 -0400

    Projects branch

---
+++ pythonProjects/mathExercise.py
@@ -0,0 +1,16 @@
+import math
+
+radius = float(input("Enter the radius of a circle: "))
+
+circumference = 2 * math.pi * radius
+area = math.pi * pow(radius, 2)
+
+print(f"The circumference is: {round(circumference, 2)}")
+print(f"The area is: {round(area, 2)}cm^2")
+
+a = float(input("Enter side A: "))
+b = float(input("Enter side B: "))
+
+c = math.sqrt(pow(a, 2) + pow(b, 2))
+
+print(f"Side C = {c}")
